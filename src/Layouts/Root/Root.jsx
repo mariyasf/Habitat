99e5cbbc129b7b6861cd656faf33df61e3bd9df5
mariya-sultana <mariasultanafahmida@gmail.com>
2024-04-21 23:54:09 +0600

    Auth added

---
+++ src/Layouts/Root/Root.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from '../../Pages/Shared/Navbar';
+import Footer from '../../Pages/Shared/Footer';
+
+const Root = () => {
+    return (
+        <div>
+            <div className="max-w-6xl mx-auto">
+                <Navbar />
+                <Outlet />
+            </div>
+            
+            <Footer />
+        </div>
+    );
+};
+
+export default Root;
